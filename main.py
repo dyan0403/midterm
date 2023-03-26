c8d7f3ffc5907f72b5fae27b89eb155bed894726
dyan0403 <127913060+dyan0403@users.noreply.github.com>
2023-03-27 01:12:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,27 @@ from comparator_factory import ComparatorFactory
 
 while True:
     try:
-        language = input("Bạn cần nhập ngôn ngữ lập trình của 2 đoạn code cần so sánhsánh để sự so sánh được chính xác hơn (python/java/c++): ").lower()
-        if ComparatorFactory.is_supported_language(language):
-            break
+        language = input("Nhập ngôn ngữ lập trình của đoạn code (python/java/c++): ").lower()
+        if not ComparatorFactory.is_supported_language(language):
+            print(f"Ngôn ngữ {language} không được hỗ trợ.")
         else:
-            print(f"Ngôn ngữ {language} không được hỗ trợ.")   #Yêu cầu người dùng nhập ngôn ngữ lập trình để so sánh
-    except:                                                 #kiểm tra xem ngôn ngữ đã nhập có được hỗ trợ bởi factory pattern hay không bằng cách gọi hàm is_supported_language. 
-        print("Đã có lỗi xảy ra, vui lòng thử lại.")
-
+            break
+    except Exception as e:
+        print(f"Đã có lỗi xảy ra: {e}")
 
 while True:
     try:
         code1 = input("Nhập đoạn code 1: ")
-        code2 = input("Nhập đoạn code 2: ")    #tạo ra một đối tượng comparator từ factory bằng cách gọi hàm create và truyền vào ngôn ngữ, đoạn code thứ nhất và thứ hai.
+        code2 = input("Nhập đoạn code 2: ")    
         break
-    except:
-        print("Đã có lỗi xảy ra, vui lòng thử lại.")
+    except Exception as e:
+        print(f"Đã có lỗi xảy ra: {e}")
 
-#Chương trình gọi phương thức compare của đối tượng comparator để so sánh hai đoạn code.
-comparator = ComparatorFactory.create(language, code1, code2)
-similarity = comparator.compare()
-print(f"Độ giống nhau của 2 đoạn code là: {similarity:.2f}%")
+try:
+    comparator = ComparatorFactory.create(language, code1, code2)
+    similarity = comparator.compare()
+    print(f"Độ giống nhau của 2 đoạn code là: {similarity:.2f}%")
+except ValueError as e:
+    print(f"Lỗi: {e}")
+except Exception as e:
+    print(f"Đã có lỗi xảy ra: {e}")
